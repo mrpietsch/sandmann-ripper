e87983187f9c138789d0e2adc189545f88f8a557
Martin Pietsch <martin@p-space.org>
2017-08-17 22:44:23 +0200

    Redirect the download stream to an S3 bucket
    The download stream is immediately been used as InputStream to an S3 bucket
    upload. You have to provide the environment variables `AWS_ACCESS_KEY` and
    `AWS_SECRET_ACCESS_KEY`.

--- src/main/kotlin/org/pspace/sandmann/Main.kt
+++ src/main/kotlin/org/pspace/sandmann/Main.kt
@@ -1,21 +1,26 @@
 package org.pspace.sandmann
 
+import com.amazonaws.auth.EnvironmentVariableCredentialsProvider
+import com.amazonaws.regions.Regions
+import com.amazonaws.services.s3.AmazonS3
+import com.amazonaws.services.s3.AmazonS3ClientBuilder
+import com.amazonaws.services.s3.model.ObjectMetadata
 import org.json.simple.JSONArray
 import org.json.simple.JSONObject
 import org.json.simple.parser.JSONParser
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Element
-import java.io.File
-import java.io.FileOutputStream
+import java.io.BufferedInputStream
+import java.io.InputStream
 import java.net.URL
-import java.nio.channels.Channels
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
 object Main {
 
-    val sandmannHomepage = URL("https://www.sandmann.de/filme/index.html")
-    val targetDirectory = File("/Users/peach/Downloads/")
+    val SANDMANN_HOMEPAGE = URL("https://www.sandmann.de/filme/index.html")
+
+    val BUCKET_NAME = "sandmann-repo"
 
     @JvmStatic
     fun main(args: Array<String>) {
@@ -23,32 +28,38 @@ object Main {
         val (todaysEpisodeDescriptor, title) = findLinkToDescriptorForTodaysEpisode()
         val bestVideoUrl = getHighestQualityVideoUrlFromJsonDescriptor(todaysEpisodeDescriptor)
 
-        val fileName = makeFileNameFromTitle(title)
-
-        val targetFile = File(targetDirectory, fileName)
-
-        println("Dowloading video from '$bestVideoUrl' to '$targetFile'")
-
-        downloadVideo(bestVideoUrl, targetFile)
-
-        println("Done!")
+        uploadVideoToS3(
+                targetFileName = makeFileNameFromTitle(title),
+                inputStream = bestVideoUrl.openStream()
+        )
     }
 
     private fun makeFileNameFromTitle(title: String): String {
         val sourceRemoved = title.replace(Regex("\\s+\\(Quelle.*\\)"), "")
         val sanitizedFilename = sanitizeFilename(sourceRemoved)
         val todayAsIso = LocalDate.now().format(DateTimeFormatter.ISO_DATE)
-        return "$todayAsIso $sanitizedFilename"
+        return "$todayAsIso $sanitizedFilename.mp4"
     }
 
     fun sanitizeFilename(name: String): String {
         return name.replace("[/:]".toRegex(), "").trim()
     }
 
-    private fun downloadVideo(url: URL, targetFile: File) {
-        val readableByteChannel = Channels.newChannel(url.openStream())
-        val outputStream = FileOutputStream(targetFile)
-        outputStream.channel.transferFrom(readableByteChannel, 0, java.lang.Long.MAX_VALUE)
+    private fun uploadVideoToS3(targetFileName: String, inputStream: InputStream) {
+        println("Upoading to AWS")
+
+        val metadata = ObjectMetadata()
+        metadata.contentType = "video/mp4"
+        metadata.contentLanguage = "de"
+
+        getS3Client().putObject(
+                BUCKET_NAME,
+                targetFileName,
+                BufferedInputStream(inputStream),
+                metadata
+        );
+
+        println("Done uploading")
     }
 
     private fun getHighestQualityVideoUrlFromJsonDescriptor(jsonDescriptorUrl: URL): URL {
@@ -75,7 +86,7 @@ object Main {
     }
 
     private fun findLinkToDescriptorForTodaysEpisode(): Pair<URL, String> {
-        val sandmannLandingPage = Jsoup.connect(sandmannHomepage.toString()).get()
+        val sandmannLandingPage = Jsoup.connect(SANDMANN_HOMEPAGE.toString()).get()
 
         val linksToVideoPages = sandmannLandingPage.select("div[data-media-ref~=/filme]")
 
@@ -88,10 +99,18 @@ object Main {
         val firstLinkElement = relevantLinksContainers.first()
 
         val title = firstLinkElement.select("img").attr("title")
-        val url = URL(sandmannHomepage, mediaRef(firstLinkElement))
+        val url = URL(SANDMANN_HOMEPAGE, mediaRef(firstLinkElement))
 
         return (url to title)
     }
 
     private fun mediaRef(element: Element) = element.attr("data-media-ref")
+
+    private fun getS3Client(): AmazonS3 {
+        return AmazonS3ClientBuilder
+                .standard()
+                .withCredentials(EnvironmentVariableCredentialsProvider())
+                .withRegion(Regions.EU_CENTRAL_1)
+                .build()
+    }
 }
